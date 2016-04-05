368a44f92a8bcabab4a7592e9c59ee1c8d5e97ee
Étienne Gélinas-Gagnon <etiennegg7@hotmail.fr>
2016-04-05 12:08:24 -0400

    ajout des classe de test

---
+++ TP4_OS2/test/test_protocole_json.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestProtocole_json(TestCase):
+    def test_interprete(self):
+        self.fail()
+
+    def test_genere_bonjour(self):
+        self.fail()
+
+    def test_genere_nom(self):
+        self.fail()
---
+++ TP4_OS2/test/test_protocole_xml.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestProtocole_xml(TestCase):
+    def test_interprete(self):
+        self.fail()
+
+    def test_genere_bonjour(self):
+        self.fail()
+
+    def test_genere_nom(self):
+        self.fail()
