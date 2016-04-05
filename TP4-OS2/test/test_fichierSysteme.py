7278971ac60fc594045df5145dd68a62c572f20e
Étienne Gélinas-Gagnon <etiennegg7@hotmail.fr>
2016-04-05 11:17:33 -0400

    Début du projet

---
+++ TP4-OS2/test/test_fichierSysteme.py
@@ -0,0 +1,5 @@
+from unittest import TestCase
+
+
+class TestFichierSysteme(TestCase):
+    pass
---
+++ TP4-OS2/test/test_protocole.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+
+class TestProtocole(TestCase):
+    def test_json(self):
+        self.fail()
+
+    def test_xml(self):
+        self.fail()
