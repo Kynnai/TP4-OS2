d9ad26f6148bf9b53a7d1424227f4661d3f1dc01
Étienne Gélinas-Gagnon <etiennegg7@hotmail.fr>
2016-04-05 12:02:47 -0400

    renommer le projet

---
+++ TP4_OS2/src/protocole_json.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import TP4_OS2.src.protocole
+
+class Protocole_json(Protocole):
+    """Interface du langage de communication JSON"""
+    def __init__(self):
+        super(Protocole_json, self).__init__()
+        pass
+
+    def interprete(self, message_serveur):
+        pass
+
+    def genere_bonjour(self):
+        pass
+
+    def genere_nom(self):
+        pass
+
+    # etc...
