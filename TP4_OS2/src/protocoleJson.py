9d12d25c7f5d5b94a1b3676c381ba330f2d8d2e9
Étienne Gélinas-Gagnon <etiennegg7@hotmail.fr>
2016-04-15 13:35:51 -0400

    Ajout des 7 premiere fonctionnalité Json

--- TP4_OS2/src/protocoleJson.py
+++ TP4_OS2/src/protocoleJson.py
@@ -14,7 +14,22 @@ class ProtocoleJson(Protocole):
         pass
 
     def genere_bonjour(self):
-        pass
+        return {"salutation":"bonjourServeur"}
 
     def genere_nom(self):
-        pass
+        return {"action":"questionNomServeur"}
+
+    def genere_listeDossiers(self, dossiers):
+        return {"questionListeDossiers":dossiers}
+
+    def genere_listeFichiers(self, fichiers):
+        return {"questionListeFichiers": fichiers}
+
+    def genere_creerDossier(self, dossier):
+        return {"creerDossier":dossier}
+
+    def genere_televerserFichier(self, nom, dossier, signature, contenu, date):
+        return {"televerserFichier":{"nom":nom, "dossier":dossier, "signature":signature, "contenu":contenu, "date":date}}
+
+    def genere_supprimerFichier(self, nom, dossier):
+        return {"supprimerFichier":{"nom":nom, "dossier":dossier}}
