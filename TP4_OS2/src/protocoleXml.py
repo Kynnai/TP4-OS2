78139988cc3593aa807bb69ffdaa5a83da756217
Frédéric Bérubé <fredob@hotmail.ca>
2016-04-19 11:20:46 -0400

    Fini les tests de protocoleXML et arrangé la variable global bugué

--- TP4_OS2/src/protocoleXml.py
+++ TP4_OS2/src/protocoleXml.py
@@ -17,50 +17,38 @@ class ProtocoleXml(Protocole):
         pass
 
     def genere_bonjour(self):
-        global PREFIXE_XML
-        return PREFIXE_XML + "<bonjourServeur />"
+        return self.PREFIXE_XML + "<bonjourServeur />"
 
     def genere_nom(self):
-        global PREFIXE_XML
-        return PREFIXE_XML + "questionNomServeur />"
+        return self.PREFIXE_XML + "<questionNomServeur />"
 
     def genere_listeDossiers(self, dossiers):
-        global PREFIXE_XML
-        return PREFIXE_XML + "<questionListeDossiers>" + dossiers + "</questionListeDossiers>"
+        return self.PREFIXE_XML + "<questionListeDossiers>" + dossiers + "</questionListeDossiers>"
 
     def genere_listeFichiers(self, fichiers):
-        global PREFIXE_XML
-        return PREFIXE_XML + "<questionListeFichiers>" + fichiers + "</questionListeFichiers>"
+        return self.PREFIXE_XML + "<questionListeFichiers>" + fichiers + "</questionListeFichiers>"
 
     def genere_creerDossier(self, dossier):
-        global PREFIXE_XML
-        return PREFIXE_XML + "<creerDossier>" + dossier + "</creerDossier>"
+        return self.PREFIXE_XML + "<creerDossier>" + dossier + "</creerDossier>"
 
     def genere_televerserFichier(self, nom, dossier, signature, contenu, date):
-        global PREFIX_XML
-        return PREFIX_XML + "<televerserFichier>" + "<nom>" + nom + "</nom>" + "<dossier>" + dossier + "</dossier>" + "<signature>" + signature + "</signature>" + \
+        return self.PREFIXE_XML + "<televerserFichier>" + "<nom>" + nom + "</nom>" + "<dossier>" + dossier + "</dossier>" + "<signature>" + signature + "</signature>" + \
             "<contenu>" + contenu + "</contenu>" + "<date>" + date + "</date>" + "</televerserFichier>"
 
     def genere_telechargerFichier(self, nom, dossier):
-        global PREFIXE_XML
-        return PREFIXE_XML + "<telechargerFichier>" + "<nom>" + nom + "</nom>" + "<dossier>" + dossier + "</dossier>" +  "</telechargerFichier>"
+        return self.PREFIXE_XML + "<telechargerFichier>" + "<nom>" + nom + "</nom>" + "<dossier>" + dossier + "</dossier>" +  "</telechargerFichier>"
 
     def genere_supprimerFichier(self, nom, dossier):
-        global PREFIXE_XML
-        return PREFIXE_XML + "<supprimerFichier>" + "<nom>" + nom + "</nom>" + "<dossier>" + dossier + "</dossier>" + "</supprimerFichier>"
+        return self.PREFIXE_XML + "<supprimerFichier>" + "<nom>" + nom + "</nom>" + "<dossier>" + dossier + "</dossier>" + "</supprimerFichier>"
 
     def genere_supprimerDossier(self, dossier):
-        global PREFIXE_XML
-        return PREFIXE_XML + "<supprimerDossier>" + dossier + "</supprimerDossier>"
+        return self.PREFIXE_XML + "<supprimerDossier>" + dossier + "</supprimerDossier>"
 
     def genere_questionFichierRecent(self, nom, dossier, date):
-        global PREFIXE_XML
-        return PREFIXE_XML + "<questionFichierRecent>" + "<nom>" + nom + "</nom>" + "<dossier>" + dossier + "</dossier>" + "<date>" + date + "</date>" + "</questionFichierRecent>"
+        return self.PREFIXE_XML + "<questionFichierRecent>" + "<nom>" + nom + "</nom>" + "<dossier>" + dossier + "</dossier>" + "<date>" + date + "</date>" + "</questionFichierRecent>"
 
     def genere_questionFichierIdentique(self, nom, dossier, signature,  date):
-        global PREFIXE_XML
-        return PREFIXE_XML + "<questionFichierRecent>" + "<nom>" + nom + "</nom>" + "<dossier>" + dossier + "</dossier>" + "<signature>" + signature + "</signature>" "<date>" + date + "</date>" + "</questionFichierRecent>"
+        return self.PREFIXE_XML + "<questionFichierRecent>" + "<nom>" + nom + "</nom>" + "<dossier>" + dossier + "</dossier>" + "<signature>" + signature + "</signature>" "<date>" + date + "</date>" + "</questionFichierRecent>"
 
     def genere_quitter(self):
-        global PREFIXE_XML
-        return PREFIXE_XML + "<quitter />"
+        return self.PREFIXE_XML + "<quitter />"
--- TP4_OS2/test/test_protocoleXml.py
+++ TP4_OS2/test/test_protocoleXml.py
@@ -1,12 +1,62 @@
 from unittest import TestCase
+from TP4_OS2.src.protocoleXml import ProtocoleXml
 
 
 class TestProtocole_xml(TestCase):
+    nom = "a1"
+    dossier = "b2"
+    dossiers = "d1/d2/d3/"
+    fichiers = "d1"
+    signature = "c3"
+    contenu = "d4"
+    date = "2016/04/19"
+
+    PREFIXE_XML = "<?xml version=\"1.0\" ?>"
+
+    def setUp(self):
+        self.protocole = ProtocoleXml()
+
     def test_interprete(self):
-        self.fail()
+        pass
 
-    def test_genere_bonjour(self):
-        self.fail()
+    def test_genere_bonjour_XML(self):
+        self.assertTrue(self.protocole.genere_bonjour(), self.PREFIXE_XML + "<bonjourServeur />")
 
-    def test_genere_nom(self):
-        self.fail()
+    def test_genere_nom_XML(self):
+        self.assertTrue(self.protocole.genere_nom(), self.PREFIXE_XML + "<questionNomServeur />")
+
+    def test_listeDossiers_XML(self):
+        self.assertTrue(self.protocole.genere_listeDossiers(self.dossiers), self.PREFIXE_XML + "<questionListeDossiers>" + self.dossiers + "</questionListeDossiers>")
+
+    def test_listeFichiers_XML(self):
+        self.assertTrue(self.protocole.genere_listeFichiers(self.fichiers), self.PREFIXE_XML + "<questionListeFichiers>" + self.fichiers + "</questionListeFichiers>")
+
+    def test_creerDossier_XML(self):
+        self.assertTrue(self.protocole.genere_creerDossier(self.dossier), self.PREFIXE_XML + "<creerDossier>" + self.dossier + "</creerDossier>")
+
+    def test_televerserFichier_XML(self):
+        self.assertTrue(self.protocole.genere_televerserFichier(self.nom, self.dossier, self.signature, self.contenu, self.date),
+                        self.PREFIXE_XML + "<televerserFichier>" + "<nom>" + self.nom + "</nom>" + "<dossier>" + self.dossier + "</dossier>" + "<signature>" + self.signature + "</signature>" + \
+                         "<contenu>" + self.contenu + "</contenu>" + "<date>" + self.date + "</date>" + "</televerserFichier>")
+
+    def test_telechargerFichier_XML(self):
+        self.assertTrue(self.protocole.genere_telechargerFichier(self.nom, self.dossier),
+                        self.PREFIXE_XML + "<telechargerFichier>" + "<nom>" + self.nom + "</nom>" + "<dossier>" + self.dossier + "</dossier>" +  "</telechargerFichier>")
+
+    def test_supprimerFichier_XML(self):
+        self.assertTrue(self.protocole.genere_supprimerFichier(self.nom, self.dossier),
+                        self.PREFIXE_XML + "<supprimerFichier>" + "<nom>" + self.nom + "</nom>" + "<dossier>" + self.dossier + "</dossier>" + "</supprimerFichier>")
+
+    def test_supprimerDossier_XML(self):
+        self.assertTrue(self.protocole.genere_supprimerDossier(self.dossier), self.PREFIXE_XML + "<supprimerDossier>" + self.dossier + "</supprimerDossier>")
+
+    def test_questionFichierRecent_XML(self):
+        self.assertTrue(self.protocole.genere_questionFichierRecent(self.nom, self.dossier, self.date),
+                        self.PREFIXE_XML + "<questionFichierRecent>" + "<nom>" + self.nom + "</nom>" + "<dossier>" + self.dossier + "</dossier>" + "<date>" + self.date + "</date>" + "</questionFichierRecent>")
+
+    def test__questionFichierIdentique(self):
+        self.assertTrue(self.protocole.genere_questionFichierIdentique(self.nom, self.dossier, self.signature, self.date),
+                        self.PREFIXE_XML + "<questionFichierRecent>" + "<nom>" + self.nom + "</nom>" + "<dossier>" + self.dossier + "</dossier>" + "<signature>" + self.signature + "</signature>" "<date>" + self.date + "</date>" + "</questionFichierRecent>")
+
+    def test_quitter_XML(self):
+        self.assertTrue(self.protocole.genere_quitter(),  self.PREFIXE_XML + "<quitter />")
