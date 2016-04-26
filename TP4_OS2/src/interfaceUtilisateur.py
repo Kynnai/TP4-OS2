a17b355b6a8b923a19f79bdaccdc0d0fa68b8895
Frédéric Bérubé <fredob@hotmail.ca>
2016-04-26 12:55:09 -0400

    Création du fichier "serveurConfig" fonctionnel
    Youpidou !

--- TP4_OS2/src/interfaceUtilisateur.py
+++ TP4_OS2/src/interfaceUtilisateur.py
@@ -27,11 +27,11 @@ class InterfaceUtilisateur:
         self.droid.dialogSetNeutralButtonText('Annuler')
         self.droid.dialogShow()
         response = self.droid.dialogGetResponse().result
-        return response['which'] in ('positive', 'negative', 'neutral')
+        return response['which']
 
     def demandeIpEtPortServeurAlert(self):
-        if os.path.exists("serveurConfig.txt"):
-            file = open("serveurConfig.txt")
+        if os.path.exists("/sdcard/com.hipipal.qpyplus/scripts3/serveurConfig.txt"):
+            file = open("/sdcard/com.hipipal.qpyplus/scripts3/serveurConfig.txt")
             lines = file.readlines()
             return (lines[0], lines[1])
         else:
@@ -42,7 +42,7 @@ class InterfaceUtilisateur:
             message2 = "Veuillez spécifier le port du serveur."
             port = self.droid.dialogGetInput(title2, message2).result
             if(self.demandeCreationFichierServeurConfig() == "positive"):
-                file = open("serveurConfig.txt")
+                file = open("/sdcard/com.hipipal.qpyplus/scripts3/serveurConfig.txt", 'w')
                 file.writelines([ip + '\n', port + '\n'])
             return (ip, port)
 
@@ -55,7 +55,7 @@ class InterfaceUtilisateur:
         self.droid.dialogSetNeutralButtonText('Annuler')
         self.droid.dialogShow()
         response = self.droid.dialogGetResponse().result
-        return response['which'] in ('positive', 'negative', 'neutral')
+        return response['which']
 
     def spinnerDeMiseAJour(self):
         title = 'Mise à jour des dossiers et fichiers avec le serveur.'
